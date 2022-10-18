53192437a87cfe2d2a80e165ae8530b4502080d9
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-10-18 10:16:01 +0200

    Style nav mobile.

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import styles from "./Navbar.module.sass";
+import Logo from "../../public/assets/icons/logo.svg";
+import Image from "next/future/image";
 
 export default function Navbar() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   return (
     <nav className={styles.nav}>
-      <button
-        type="button"
-        onClick={() => setMenuIsOpen(!menuIsOpen)}
-        className={styles.hamburgerButton}
-      >
+      <Image className={styles.logo} src={Logo}></Image>
+
+      <button type="button" name="Meny" onClick={() => setMenuIsOpen(!menuIsOpen)} className={styles.hamburgerButton}>
         <div className={styles.barsContainer}>
           <div className={styles.hamburgerBar}></div>
           <div className={styles.hamburgerBar}></div>
           <div className={styles.hamburgerBar}></div>
         </div>
-        <p>Meny</p>
+        Meny
       </button>
     </nav>
   );
