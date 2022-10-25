04ebcadfa057f5fc87d3f2e9e5b1e07250db52ec
h-emma <emmah92@hotmail.com>
2022-10-25 14:08:47 +0200

    Add preloader for font

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="sv">
-      <Head />
+      <Head>
+        {/* <link
+          rel="preload"
+          href="/assets/fonts/Poppins-Regular.woff"
+          as="font"
+          type="font/woff"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/assets/fonts/Poppins-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossorigin=""
+        /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
