483907e3d940c8cd71aca72b3e844b8fc1a2eec3
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-11-09 10:38:04 +0100

    Change form to registration. Change order on startSection.

--- components/Items/FormItem/FormItem.jsx
+++ components/Items/FormItem/FormItem.jsx
@@ -12,22 +12,28 @@ export default function FormItem(itemList) {
         }}
         className={styles.form}
       >
-        <h4>Logga in</h4>
+        <h4>Registrera dig</h4>
         <fieldset className={styles.fieldset}>
-          <legend>Inloggningsdetaljer</legend>
+          <legend>Registreringsdetaljer</legend>
+          <p>Fält med * måste fyllas i.</p>
           <div className={styles.inputAndLabelContainer}>
             <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" id="username" required />
+            <div>
+              <input type="email" name="email" id="email" required /> *
+            </div>
           </div>
           <div className={styles.inputAndLabelContainer}>
             <label htmlFor="password">Lösenord</label>
-            <input type="password" name="password" id="password" required minLength="6" />
+            <div>
+              <input type="password" name="password" id="password" required minLength="8" /> *
+            </div>
+            <p>Lösenord måste vara minst 8 tecken.</p>
           </div>
         </fieldset>
-        <button type="submit">Logga in</button>
+        <button type="submit">Registrera</button>
         <div className={styles.signUpContainer}>
-          <p>Inget konto?</p>
-          <a href="">Registrera dig här</a>
+          <p>Redan registrerad?</p>
+          <a href="">Logga in</a>
         </div>
       </form>
     </ItemCard>
--- components/StartSection/StartSection.jsx
+++ components/StartSection/StartSection.jsx
@@ -6,7 +6,14 @@ import styles from "./StartSection.module.sass";
 const StartSection = react.forwardRef((props, ref) => {
   return (
     <section className={styles.startSection}>
-      <div className={styles.itemListContainer}>
+      <article className={styles.article}>
+        <div className={styles.articleHeaderContainer}>
+          <h1>Tillgänglighet</h1>
+        </div>
+        <StartSectionText />
+      </article>
+
+      <section className={styles.itemListContainer}>
         <h2 tabIndex={0} ref={ref}>
           Index
         </h2>
@@ -122,13 +129,7 @@ const StartSection = react.forwardRef((props, ref) => {
             </Link>
           </li>
         </ul>
-      </div>
-      <article className={styles.article}>
-        <div className={styles.articleHeaderContainer}>
-          <h1>Tillgänglighet</h1>
-        </div>
-        <StartSectionText />
-      </article>
+      </section>
     </section>
   );
 });
