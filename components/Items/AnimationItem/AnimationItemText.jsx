fce14364b73fe291e85923713483db62302c7c56
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-11-13 11:21:05 +0100

    Add animation text.

--- components/Items/AnimationItem/AnimationItemText.jsx
+++ components/Items/AnimationItem/AnimationItemText.jsx
@@ -1,17 +1,59 @@
 import styles from "../../ItemCard/ItemCard.module.sass";
 export default function AnimationItemText() {
   return (
-    <p>
-      A text about animation, consectetur adipiscing elit. Quisque auctor ipsum eget mollis sollicitudin. Pellentesque
-      id tortor non eros condimentum placerat. Etiam aliquet magna id libero viverra, id ullamcorper erat hendrerit. In
-      hac habitasse platea dictumst. Curabitur ullamcorper laoreet sodales. Nulla quis malesuada nulla. Cras vitae
-      interdum tortor. Nam blandit semper consectetur.
-      <br />
-      <br />
-      Sed convallis mi ut leo accumsan, ut venenatis purus varius. Aliquam a consequat erat, in tristique ex. Sed sit
-      amet scelerisque nisl, in molestie sapien. Vivamus id finibus lectus. Nam tincidunt, enim at placerat vestibulum,
-      diam dolor molestie sem, ut imperdiet justo diam eget metus. Pellentesque ligula massa, dapibus vitae ultrices
-      nec, ornare vitae lacus.
-    </p>
+    <>
+      <p>
+        Rörligt innehåll på en hemsida kan vara ett viktigt element för att skapa olika sorters känslor för besökaren.
+        Det kan även användas för att visa information samt tydliggöra övergångar mellan olika delar av en hemsida.
+        Beroende på vad en hemsida försöker uppnå samt hur information visas kan det vara svårt att dra alltför exakta
+        regler för vad som blir tillgängligt för olika användare. Men det är bra att ha i åtanke att rörelse som
+        uppfattas tillgängligt för en användare, kan kännas distraherande och överväldigande för en annan.
+      </p>
+      <p>Nedan följer generella regler för rörligt innehåll.</p>
+
+      <h4>Play Stop Paus</h4>
+      <p>
+        Enligt WCAG 2.1 ska all information som på något sätt rör sig, blinkar eller scrollar kunna stängas av, döljas
+        eller pausas. Detta gäller dock bara om allt av det följande är sant:
+      </p>
+      <p>
+        Att det som rör sig startar automatiskt. Att det håller på längre än 5 sekunder. Att det presenteras tillsammans
+        med annat innehåll på sidan.
+      </p>
+      <p>
+        Det finns också regler att följa gällande blinkande innehåll, där en gräns på tre blinkningar per sekund inte
+        ska överskridas, då de kan framkalla epileptiska anfall hos en del användare.
+      </p>
+      <p>Dessa regler gäller för allt rörligt innehåll, såsom CSS-animationer, GIF-bilder och video-innehåll.</p>
+      <p>
+        Tänk på att text som rör sig kan vara svårt att ta till sig om man som användare behöver tid på sig för att
+        läsa.
+      </p>
+      <h4>Prefers reduced motion</h4>
+      <p>
+        Precis som man kan använda en @media-query för att göra ändringar i css beroende på till exempel skärmstorlek,
+        kan man även göra en alternativ css-layout beroende på om användaren använder sig av inställningen “prefers
+        reduced motion.” Detta ställs vanligtvis in direkt i användarens operativsystem. Då kan man som programmerare
+        välja att till exempel stänga av eller tona ner animationer som inte är direkt nödvändiga för sidans innehåll.
+      </p>
+      <p className={styles.sourceParagraph}>
+        Källa:{""}
+        <a href="https://www.w3.org/WAI/WCAG21/Understanding/pause-stop-hide.html">
+          https://www.w3.org/WAI/WCAG21/Understanding/pause-stop-hide.html
+        </a>
+      </p>
+      <p className={styles.sourceParagraph}>
+        Källa:{""}
+        <a href="https://www.w3.org/TR/WCAG21/#seizures-and-physical-reactions">
+          https://www.w3.org/TR/WCAG21/#seizures-and-physical-reactions
+        </a>
+      </p>
+      <p className={styles.sourceParagraph}>
+        Källa:{""}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion">
+          https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
+        </a>
+      </p>
+    </>
   );
 }
