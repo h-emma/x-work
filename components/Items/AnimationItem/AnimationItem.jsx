e738be53695f156435ba8f8de620556cf89f8b9f
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-11-13 12:25:01 +0100

    Add some function to button.

--- components/Items/AnimationItem/AnimationItem.jsx
+++ components/Items/AnimationItem/AnimationItem.jsx
@@ -1,12 +1,22 @@
 import ItemCard from "../../ItemCard/ItemCard";
 import styles from "./AnimationItem.module.sass";
 import AnimationItemText from "./AnimationItemText";
+import { useState, useRef } from "react";
 
 export default function AnimationItem(itemList) {
+  const animatedShape = useRef(null);
+
+  const [animationIsPlaying, setAnimationIsPlaying] = useState(true);
+
   return (
     <ItemCard id="animation-item" name="Animationer" text={<AnimationItemText />} itemList={itemList}>
-      <div className={styles.animationContainer}>
-        <div className={styles.animatedShape}></div>
+      <div className={styles.animationCardContentContainer}>
+        <div className={styles.animationContainer}>
+          <div className={styles.animatedShape} ref={animatedShape}></div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button onClick={() => {}}>Spela upp/Pausa</button>
+        </div>
       </div>
     </ItemCard>
   );
