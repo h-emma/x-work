e66eba03855795bfa6c6f8b83b942ac6acb1e45a
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-11-15 22:18:06 +0100

    Add animation button function. Change color nav.

--- components/Items/AnimationItem/AnimationItem.jsx
+++ components/Items/AnimationItem/AnimationItem.jsx
@@ -1,21 +1,25 @@
 import ItemCard from "../../ItemCard/ItemCard";
 import styles from "./AnimationItem.module.sass";
 import AnimationItemText from "./AnimationItemText";
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export default function AnimationItem(itemList) {
-  const animatedShape = useRef(null);
-
   const [animationIsPlaying, setAnimationIsPlaying] = useState(true);
 
   return (
     <ItemCard id="animation-item" name="Animationer" text={<AnimationItemText />} itemList={itemList}>
       <div className={styles.animationCardContentContainer}>
         <div className={styles.animationContainer}>
-          <div className={styles.animatedShape} ref={animatedShape}></div>
+          <div className={animationIsPlaying ? styles.animatedShape : styles.pausedAnimatedShape}></div>
         </div>
         <div className={styles.buttonContainer}>
-          <button onClick={() => {}}>Spela upp/Pausa</button>
+          <button
+            onClick={() => {
+              setAnimationIsPlaying((current) => !current);
+            }}
+          >
+            Spela upp/Pausa
+          </button>
         </div>
       </div>
     </ItemCard>
