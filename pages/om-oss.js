b433c9245810b303cf1d6eb27af3b4dcd3765d2c
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-11-15 11:54:06 +0100

    Add about us text.

--- pages/om-oss.js
+++ pages/om-oss.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useEffect } from "react";
+import styles from "../styles/AboutAccessibility.module.sass";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
@@ -16,8 +17,20 @@ export default function AboutUs() {
       </Head>
 
       <Navbar backgroundColor="white" pageIndex={1}></Navbar>
-      <main>
-        <h1>Hej</h1>
+      <main className={styles.main}>
+        <h1>Om oss</h1>
+
+        <p>
+          Denna sida är skapad av två studerande, Albin och Emma, på webbutvecklarutbildningen på Yrgo i Göteborg
+          HT2022. Hemsidan är en del utav vårt examensarbete. Vårt mål med hemsidan är att bidra med kunskap som
+          underlättar för andra utvecklare att skapa en mer tillgänglig webb, en webb för alla!
+        </p>
+        <p>
+          Under projektperiodens sex veckor har vi lärt oss en hel del om tillgänglighet på webben. Det som vi tar med
+          oss mest är att en vision på 100% tillgänglighet är orimligt och att även Goodenough kan leda till ett
+          Nobelpris.
+        </p>
+        <div className={styles.emptySpace}></div>
       </main>
       <Footer></Footer>
     </div>
