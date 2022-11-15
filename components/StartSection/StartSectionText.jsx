f0eee59faee63c3b2cb910c676fcd34d23eb7b6a
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-11-15 13:19:38 +0100

    Update StartSectionText.

--- components/StartSection/StartSectionText.jsx
+++ components/StartSection/StartSectionText.jsx
@@ -6,9 +6,9 @@ export default function ButtonItem() {
       Tillgänglighet eller accessibility som det heter på engelska är en viktig del av webbens funktion. För att kunna
       erbjuda webben för alla behöver utvecklare tänka på tillgänglighet när en utvecklar en hemsida. Men vad är det då
       man ska tänka på? Är det krångligt eller behöver man tumma på det grafiska uttrycket? Nedan följer en lista på
-      vanliga element och komponenter och hur dessa kan kodas för att förbättra tillgängligheten. Under{" "}
-      <i>Om tillgänglighet</i> finns det mer att läsa om varför ämnet är viktigt, vad som är bra att tänka på vid
-      skapande av en tillgänglig webb samt länkar till mer information om ämnet.
+      vanliga element, komponenter samt andra saker som kan vara bra att tänka på och hur dessa kan kodas för att
+      förbättra tillgängligheten. Under <i>Om tillgänglighet</i> finns det mer att läsa om varför ämnet är viktigt, vad
+      som är bra att tänka på vid skapande av en tillgänglig webb samt länkar till mer information om ämnet.
     </p>
   );
 }
