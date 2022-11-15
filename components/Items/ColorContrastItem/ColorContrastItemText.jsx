06bf4adb89b90d8ac1eab8b6c147c4900187797e
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-11-15 14:35:54 +0100

    Remove Color from Contrast Checker.

--- components/Items/ColorContrastItem/ColorContrastItemText.jsx
+++ components/Items/ColorContrastItem/ColorContrastItemText.jsx
@@ -3,29 +3,21 @@ export default function ColorContrastItemText() {
   return (
     <>
       <p>
-        Färgkontrast är en stor del av att tillgängliggöra en hemsida. För att
-        underlätta läsningen för användare med tillexempel färgblindhet är det
-        viktigt att färgen på texten och dess bakgrund har en så bra kontrast
-        som möjligt. Enligt WCAG 2.0 är en bra kontrast på brödtext en ratio på
-        minst 4.5:1 för att uppnå nivå AA och 4:1 för att uppnå nivå AAA. För
-        test som har 18.5px och är bold samt 24px och större är kraven på nivå
-        AA 3.1 i kontrast och för AAA 4:5.1. Dessa riktlinjer gäller inte
-        dekorativa texter och logotyper. Gällande färgkontraster på delar som
-        inte är text så som formulär behöver ration vara mellan 3 till 1.
+        Färgkontrast är en stor del av att tillgängliggöra en hemsida. För att underlätta läsningen för användare med
+        tillexempel färgblindhet är det viktigt att färgen på texten och dess bakgrund har en så bra kontrast som
+        möjligt. Enligt WCAG 2.0 är en bra kontrast på brödtext en ratio på minst 4.5:1 för att uppnå nivå AA och 4:1
+        för att uppnå nivå AAA. För test som har 18.5px och är bold samt 24px och större är kraven på nivå AA 3.1 i
+        kontrast och för AAA 4:5.1. Dessa riktlinjer gäller inte dekorativa texter och logotyper. Gällande
+        färgkontraster på delar som inte är text så som formulär behöver ration vara mellan 3 till 1.
       </p>
       <p>
-        På webben finns det en del verktyg som underlättar testning av
-        färgkontraster och deras ratio. Ett sådant verktyg är WebAIM’s{""}
-        <a href="https://webaim.org/resources/contrastchecker/">
-          Color Contrast Checker
-        </a>
-        .
+        På webben finns det en del verktyg som underlättar testning av färgkontraster och deras ratio. Ett sådant
+        verktyg är WebAIM’s{""}
+        <a href="https://webaim.org/resources/contrastchecker/">Contrast Checker</a>.
       </p>
       <p className={styles.sourceParagraph}>
         Källa:{""}
-        <a href="https://webaim.org/resources/contrastchecker/">
-          https://webaim.org/resources/contrastchecker/
-        </a>
+        <a href="https://webaim.org/resources/contrastchecker/">https://webaim.org/resources/contrastchecker/</a>
       </p>
       <p className={styles.sourceParagraph}>
         Källa:{""}
